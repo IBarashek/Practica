9dd2dd2c99cd1e1f5fb819bd28b2226affe324c9
IBarashek <yuphighlight@mail.ru>
2025-03-27 16:56:27 +0300

    d

---
+++ Practica/pages/ChooseGroupPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Practica.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ChooseGroupPage.xaml
+    /// </summary>
+    public partial class ChooseGroupPage : Page
+    {
+        ConnectionClass connection = new ConnectionClass();
+        public ChooseGroupPage(string group)
+        {
+            int g = Convert.ToInt32(group);
+            InitializeComponent();
+            DgStudents.ItemsSource = connection.entities.Student.Where(x => x.Id_Group == 
+            (connection.entities.Group.Where(y => g == y.Name).FirstOrDefault().Id_Group)).ToList();
+        }
+    }
+}
--- Practica/pages/GroupPage.xaml.cs
+++ Practica/pages/GroupPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Practica.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,24 @@ namespace Practica.pages
     /// </summary>
     public partial class GroupPage : Page
     {
+        ConnectionClass connection = new ConnectionClass();
         public GroupPage()
         {
             InitializeComponent();
+            LtvProgramming.ItemsSource = connection.entities.Group.Where(x => x.Id_NameOfGroup == 1).Select(
+                x => x.Name).ToList();
+            LtvPhusicalCulture.ItemsSource = connection.entities.Group.Where(x => x.Id_NameOfGroup == 4).Select(
+                x =>  x.Name ).ToList();
+            LtvPrepodavN.ItemsSource = connection.entities.Group.Where(x => x.Id_NameOfGroup == 2).Select(
+                x =>  x.Name ).ToList();
+            LtvPreSchool.ItemsSource = connection.entities.Group.Where(x => x.Id_NameOfGroup == 3).Select(
+                x => x.Name ).ToList();
+        }
+
+        void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListViewItem listViewItem = sender as ListViewItem;
+            NavigationService.Navigate(new ChooseGroupPage(listViewItem.Content.ToString()));
         }
     }
 }
